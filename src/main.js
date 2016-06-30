31dd4b1abc925dfde10fe6fa406b1151ba505613
Dale Race <race.dale@gmail.com>
2016-06-30 14:32:14 -0500

    Indention

--- src/main.js
+++ src/main.js
@@ -7,48 +7,48 @@ import spawnManager from 'spawnManager';
 
 module.exports.loop = function () {
 
-// Clean memory for expired creeps
-for (let name in Memory.creeps) {
-  if (Game.creeps[name] == undefined) {
-    delete Memory.creeps[name];
+  // Clean memory for expired creeps
+  for (let name in Memory.creeps) {
+    if (Game.creeps[name] == undefined) {
+      delete Memory.creeps[name];
+    }
   }
-}
 
-// Only run spawnManager if there is enough energy
-if (Game.spawns.spawn1.spawning === null) {
-  if (Game.spawns.spawn1.energy >= 300) {
-    spawnManager();
+  // Only run spawnManager if there is enough energy
+  if (Game.spawns.spawn1.spawning === null) {
+    if (Game.spawns.spawn1.energy >= 300) {
+      spawnManager();
+    }
   }
-}
 
-for(let name in Game.creeps) {
-  let creep = Game.creeps[name];
-  if(creep.memory.role == 'harvester') {
-    roleHarvester.run(creep);
+  for(let name in Game.creeps) {
+    let creep = Game.creeps[name];
+    if(creep.memory.role == 'harvester') {
+      roleHarvester.run(creep);
+    }
+    if(creep.memory.role == 'upgrader') {
+      roleUpgrader.run(creep);
+    }
+    if(creep.memory.role == 'builder') {
+      roleBuilder.run(creep);
+    }
+    // console.log("For loop:", Game.getUsedCPU());
   }
-  if(creep.memory.role == 'upgrader') {
-    roleUpgrader.run(creep);
-  }
-  if(creep.memory.role == 'builder') {
-    roleBuilder.run(creep);
-  }
-  // console.log("For loop:", Game.getUsedCPU());
-}
 
-// Tower code from tutorial
-const towerStructure = Game.getObjectById('TOWER_ID');
-if(towerStructure) {
- let closestDamagedStructure = towerStructure.pos.findClosestByRange(FIND_STRUCTURES,
-   {
-   filter: (structure) => structure.hits < structure.hitsMax
-   });
- if(closestDamagedStructure) {
-   towerStructure.repair(closestDamagedStructure);
- }
+  // Tower code from tutorial
+  const towerStructure = Game.getObjectById('TOWER_ID');
+  if(towerStructure) {
+   let closestDamagedStructure = towerStructure.pos.findClosestByRange(FIND_STRUCTURES,
+     {
+     filter: (structure) => structure.hits < structure.hitsMax
+     });
+   if(closestDamagedStructure) {
+     towerStructure.repair(closestDamagedStructure);
+   }
 
- let closestHostile = towerStructure.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
- if(closestHostile) {
-   towerStructure.attack(closestHostile);
- }
-}
+   let closestHostile = towerStructure.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+   if(closestHostile) {
+     towerStructure.attack(closestHostile);
+   }
+  }
 }
