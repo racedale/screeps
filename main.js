21e4b925245fa93e80500f263952706be8ecd5a5
Dale Race <race.dale@gmail.com>
2016-06-29 20:01:16 -0500

    Add auto build harvesters, then guards, then healers

--- main.js
+++ main.js
@@ -3,7 +3,27 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
+  var harvesters = [];
+  var guards = [];
+  var builders = [];
+  var healers = [];
 
+  if(harvesters.length < 4) {
+      Game.spawns.spawn1.createCreep([Game.WORK, Game.CARRY, Game.MOVE], null, {role: 'harvester'});
+  } else {
+
+      if ((guards.length + healers.length) / harvesters.length < 0.8) {
+          if (guards.length < 1) {
+              Game.spawns.spawn1.createCreep([Game.TOUGH, Game.MOVE, Game.ATTACK, Game.MOVE, Game.ATTACK], null, {role: 'guard'});
+          } else if (healers.length / guards.length < 0.5) {
+              Game.spawns.spawn1.createCreep([Game.HEAL, Game.MOVE], null, {role: 'healer'});
+          } else {
+              Game.spawns.spawn1.createCreep([Game.TOUGH, Game.MOVE, Game.ATTACK, Game.MOVE, Game.ATTACK], null, {role: 'guard'});
+          }
+      } else {
+          Game.spawns.spawn1.createCreep([Game.WORK, Game.CARRY, Game.MOVE], null, {role: 'harvester'});
+      }
+  }
     var tower = Game.getObjectById('TOWER_ID');
     if(tower) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
