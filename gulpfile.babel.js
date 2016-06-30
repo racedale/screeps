66145372cb0aa20962f46c7424dfc19cafdd0b08
Dale Race <race.dale@gmail.com>
2016-06-30 08:48:09 -0500

    Add watch task for JS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,18 +3,13 @@ import gulp from 'gulp'; // ES6 imports!
 import babel from 'gulp-babel';
 
 gulp.task('js', () => { // Arrow functions!!
-  // return gulp.src('src/**/*.js')
-	// 	.pipe(babel({
-	// 		presets: ['es2015']
-	// 	}))
-	// 	.pipe(gulp.dest('./'))
-});
-
- gulp.task('default', () => {
-  //  gulp.watch('./src/**/*.js', ['js']);
   return gulp.src('src/**/*.js')
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(gulp.dest('dist'))
+});
+
+ gulp.task('default', () => {
+   gulp.watch('./src/**/*.js', ['js']);
  });
