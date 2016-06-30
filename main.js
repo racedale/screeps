af03b48b1b1a762da3a8a7caca892df0ef3e5427
Dale <race.dale@gmail.com>
2016-06-29 22:09:46 -0500

    Only try to spawn new units if >= 300 energy

--- main.js
+++ main.js
@@ -9,43 +9,44 @@ module.exports.loop = function () {
   var healers = [];
   var guards = [];
 
-
-  if(harvesters.length < 4) {
-      Game.spawns.spawn1.createCreep([WORK,CARRY,MOVE], null, {role: 'harvester'});
-      console.log('Spawning new harvester');
-  } else if (upgraders.length < 2) {
-     Game.spawns.spawn1.createCreep([WORK, WORK, CARRY, MOVE], null, {role: 'upgrader'});
-     console.log('Spawning new upgrader');
-  } else if (builders.length < 2) {
-     Game.spawns.spawn1.createCreep([WORK, WORK, CARRY, MOVE], null, {role: 'builder'});
-     console.log('Spawning new builder');
+  if (Game.spawns.spawn1.energy >= 300) {
+    if(harvesters.length < 4) {
+        Game.spawns.spawn1.createCreep([WORK,CARRY,MOVE], null, {role: 'harvester'});
+        console.log('Spawning new harvester');
+    } else if (upgraders.length < 2) {
+       Game.spawns.spawn1.createCreep([WORK, WORK, CARRY, MOVE], null, {role: 'upgrader'});
+       console.log('Spawning new upgrader');
+    } else if (builders.length < 2) {
+       Game.spawns.spawn1.createCreep([WORK, WORK, CARRY, MOVE], null, {role: 'builder'});
+       console.log('Spawning new builder');
+    }
   }
 
-    var tower = Game.getObjectById('TOWER_ID');
-    if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
-
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower.attack(closestHostile);
-        }
+  var tower = Game.getObjectById('TOWER_ID');
+  if(tower) {
+    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: (structure) => structure.hits < structure.hitsMax
+    });
+    if(closestDamagedStructure) {
+        tower.repair(closestDamagedStructure);
     }
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if(closestHostile) {
+        tower.attack(closestHostile);
     }
+  }
+
+  for(var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    if(creep.memory.role == 'harvester') {
+        roleHarvester.run(creep);
+    }
+    if(creep.memory.role == 'upgrader') {
+        roleUpgrader.run(creep);
+    }
+    if(creep.memory.role == 'builder') {
+        roleBuilder.run(creep);
+    }
+  }
 }
