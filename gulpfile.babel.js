0bd400b014073d821c9ebca9e669665a9e154ee6
Dale Race <race.dale@gmail.com>
2016-06-30 08:38:57 -0500

    Put dist files in root

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,11 +3,11 @@ import gulp from 'gulp'; // ES6 imports!
 import babel from 'gulp-babel';
 
 gulp.task('js', () => { // Arrow functions!!
-  return gulp.src('src/**/*.js')
-		.pipe(babel({
-			presets: ['es2015']
-		}))
-		.pipe(gulp.dest('dist'))
+  // return gulp.src('src/**/*.js')
+	// 	.pipe(babel({
+	// 		presets: ['es2015']
+	// 	}))
+	// 	.pipe(gulp.dest('./'))
 });
 
  gulp.task('default', () => {
@@ -16,5 +16,5 @@ gulp.task('js', () => { // Arrow functions!!
     .pipe(babel({
       presets: ['es2015']
     }))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('./'))
  });
---
+++ role.upgrader.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('role.upgrader');
+ * mod.thing == 'a thing'; // true
+ */
+
+var roleUpgrader = {
+
+    /** @param {Creep} creep **/
+    run: function run(creep) {
+
+        if (creep.memory.upgrading && creep.carry.energy == 0) {
+            creep.memory.upgrading = false;
+        }
+        if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.upgrading = true;
+        }
+
+        if (creep.memory.upgrading) {
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+            }
+        } else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+        }
+    }
+};
+
+module.exports = roleUpgrader;
