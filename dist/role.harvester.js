82638588ca09802c4353c17ceee26ba65f2df972
Dale Race <race.dale@gmail.com>
2016-07-02 08:08:39 -0500

    comment out thPion code

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -8,6 +8,12 @@ exports.default = {
   /** @param {Creep} creep **/
 
   run: function run(creep) {
+    // if (creep.memory.working == true && creep.carry.energy == 0) {
+    //   creep.memory.working = false;
+    // } else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+    //   creep.memory.working = true;
+    // }
+
     if (creep.carry.energy < creep.carryCapacity) {
       var source = creep.pos.findClosestByRange(FIND_SOURCES);
       if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
--- src/role.harvester.js
+++ src/role.harvester.js
@@ -3,6 +3,12 @@ export default {
 
  /** @param {Creep} creep **/
   run(creep) {
+    // if (creep.memory.working == true && creep.carry.energy == 0) {
+    //   creep.memory.working = false;
+    // } else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+    //   creep.memory.working = true;
+    // }
+
    if(creep.carry.energy < creep.carryCapacity) {
      let source = creep.pos.findClosestByRange(FIND_SOURCES);
      if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
