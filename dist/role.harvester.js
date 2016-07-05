2465f9fc79732873a699319b5afcaa750bd04625
Dale Race <race.dale@gmail.com>
2016-07-05 15:18:07 -0500

    change harvest to transfer

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -29,7 +29,7 @@ exports.default = {
         var _source = creep.pos.findClosestByRange(FIND_SOURCES);
       }
 
-      if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+      if (creep.transfer(source) == ERR_NOT_IN_RANGE) {
         creep.moveTo(source);
       }
     } else {
--- src/role.harvester.js
+++ src/role.harvester.js
@@ -22,7 +22,7 @@ export default {
       let source = creep.pos.findClosestByRange(FIND_SOURCES);
     }
 
-    if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+    if(creep.transfer(source) == ERR_NOT_IN_RANGE) {
        creep.moveTo(source);
      }
    }
