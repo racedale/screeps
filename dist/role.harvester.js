61fc878e9205472954fa5b83bf851672ce0c0a9b
Dale Race <race.dale@gmail.com>
2016-07-05 15:41:51 -0500

    Revert to old harvesters

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -8,24 +8,20 @@ exports.default = {
   /** @param {Creep} creep **/
 
   run: function run(creep) {
-
+    // if (creep.memory.working == true && creep.carry.energy == 0) {
+    //   creep.memory.working = false;
+    // } else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+    //   creep.memory.working = true;
+    // }
     var container = creep.room.find(FIND_MY_STRUCTURES, { filter: function filter(structure) {
         return structure.structureType == STRUCTURE_CONTAINER && structure.energy > 0;
       } });
 
     if (creep.carry.energy < creep.carryCapacity) {
 
-      var source = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter: function filter(structure) {
-          return structure.structureType == STRUCTURE_CONTAINER; //TODO: check for energy
-        }
-      });
+      var source = creep.pos.findClosestByRange(FIND_SOURCES);
 
-      if (source == undefined) {
-        var _source = creep.pos.findClosestByRange(FIND_SOURCES);
-      }
-
-      if (source[0].transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+      if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
         creep.moveTo(source);
       }
     } else {
--- src/role.harvester.js
+++ src/role.harvester.js
@@ -3,22 +3,18 @@ export default {
 
  /** @param {Creep} creep **/
   run(creep) {
-
+    // if (creep.memory.working == true && creep.carry.energy == 0) {
+    //   creep.memory.working = false;
+    // } else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+    //   creep.memory.working = true;
+    // }
     let container = creep.room.find(FIND_MY_STRUCTURES, { filter: (structure) => structure.structureType == STRUCTURE_CONTAINER && structure.energy > 0});
 
   if(creep.carry.energy < creep.carryCapacity) {
 
-    let source = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-     filter: (structure) => {
-       return structure.structureType == STRUCTURE_CONTAINER; //TODO: check for energy
-     }
-    })
+    let source = creep.pos.findClosestByRange(FIND_SOURCES);
 
-    if (source == undefined) {
-      let source = creep.pos.findClosestByRange(FIND_SOURCES);
-    }
-
-    if(source[0].transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+    if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
        creep.moveTo(source);
      }
    }
