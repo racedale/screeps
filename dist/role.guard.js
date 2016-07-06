54faec820bae084838759083bea9d7586039b2b0
Dale <race.dale@gmail.com>
2016-07-05 22:05:11 -0500

    Don't attack Graham

--- dist/role.guard.js
+++ dist/role.guard.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -9,11 +9,15 @@ exports.default = {
 
   run: function run(creep) {
 
-    var movePos = new RoomPosition(45, 25, 'W33S39');
+    var movePos = new RoomPosition(45, 25, 'W32S39'); //East side of Graham's room
     var target = creep.pos.findClosestByRange(Game.HOSTILE_CREEPS);
-    if (target && creep.hits > creep.hitsMax - 500 /* no more attack */) {
-        creep.moveTo(target);
-        creep.attack(target);
+    console.log(target.owner.username);
+
+    if (target && creep.hits > creep.hitsMax - 200 /* no more attack */) {
+        if (target.owner.username != "Graham") {
+          creep.moveTo(target);
+          creep.attack(target);
+        }
       } else {
       creep.moveTo(movePos);
     }
--- src/role.guard.js
+++ src/role.guard.js
@@ -4,11 +4,15 @@ export default {
   /** @param {Creep} creep **/
   run(creep) {
 
-    let movePos = new RoomPosition(45, 25, 'W33S39');
+    let movePos = new RoomPosition(45, 25, 'W32S39'); //East side of Graham's room
     let target = creep.pos.findClosestByRange(Game.HOSTILE_CREEPS);
-  	if(target && creep.hits > creep.hitsMax - 500 /* no more attack */) {
-  		creep.moveTo(target);
-  		creep.attack(target);
+    console.log(target.owner.username);
+
+  	if(target && creep.hits > creep.hitsMax - 200 /* no more attack */) {
+      if (target.owner.username != "Graham") {
+        creep.moveTo(target);
+        creep.attack(target);
+      }
   	} else {
   		creep.moveTo(movePos);
   	}
